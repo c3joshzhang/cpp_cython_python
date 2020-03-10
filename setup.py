9d453d4ca4a618a1dc0ae2ab58f4e800c9790f75
josh Zhang <josh.zhang@c3iot.com>
2020-03-10 14:42:04 -0700

    into installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="cpp_ext",
+    version="0.0.0",
+    author="Josh Zhang",
+    description="Example for intergrating C++ and Python",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.5',
+)
